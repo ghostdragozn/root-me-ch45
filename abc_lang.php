0ee6ef668ba0df2f2d70ac08cd9a7d75cc07babb
Trần Văn Đông <ghostdragon2596@gmail.com>
2018-08-23 13:57:15 +0700

    Update abc_lang.php

--- abc_lang.php
+++ abc_lang.php
@@ -1,10 +1,3 @@
 <?php
-if ($handle = opendir('.')) {
-    while (false !== ($entry = readdir($handle))) {
-        if ($entry != "." && $entry != "..") {
-            echo "$entry\n";
-        }
-    }
-    closedir($handle);
-}
+echo file_get_contents("index.php");
 ?>
